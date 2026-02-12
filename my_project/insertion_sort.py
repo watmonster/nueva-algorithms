148f4cebe21cbef0f1952d4c8426d900c0693f80
watmonster <112777372+watmonster@users.noreply.github.com>
2026-02-11 20:11:05 -0800

    it works now

--- my_project/insertion_sort.py
+++ my_project/insertion_sort.py
@@ -5,30 +5,36 @@ import random
 
 def make_input(len):
     my_input = [x for x in range(1, len + 1)]
-    # print(my_input)
     random.shuffle(my_input)
     return my_input
 
 
+def list_to_spaces(my_list):
+    the_str = ""
+    for item in my_list:
+        the_str += str(item)
+        the_str += " "
+    return the_str
+
+
 def insertion_sort(my_list):
     for i in range(1, len(my_list)):
-        # print("i ", i)
+        new_i = i
         for j in range(i - 1, -1, -1):  # tested: this works!
-            # print(list(range(i - 1, -1, -1)))
-            new_i = i
-            # print(new_i)
             if my_list[new_i] >= my_list[j]:
-                print(my_list)
                 break
             elif my_list[new_i] < my_list[j]:
                 my_list[new_i], my_list[j] = my_list[j], my_list[new_i]
-                print(my_list)
-                # print(new_i)
-                new_i = new_i - 1
-                # print(new_i)
+                print(list_to_spaces(my_list))
+                new_i -= 1
     return my_list
 
 
-my_test_list = make_input(6)
-print("my_test_list", my_test_list)
-print(insertion_sort(my_test_list))
+my_len = 50
+my_test_list = make_input(my_len)
+# print("my_test_list", my_test_list)
+my_sorted_list = insertion_sort(my_test_list)
+if my_sorted_list == [x for x in range(1, my_len + 1)]:
+    print("yay")
+else:
+    print("not yay")
