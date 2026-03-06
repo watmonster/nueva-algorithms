368bc5f40c4dc564b8fd5151a737063800c74db0
watmonster <112777372+watmonster@users.noreply.github.com>
2026-03-05 20:45:52 -0800

    deletion works for realsies

--- my_project/treaps.py
+++ my_project/treaps.py
@@ -36,28 +36,43 @@ def simple_insert(root_node, value):
 
 def simple_search(root_node, value):
     if value == root_node.value:
+        print("found it")
         return root_node
     elif value < root_node.value and root_node.left_child:
+        print("traversing down left")
         return simple_search(root_node.left_child, value)
     elif value > root_node.value and root_node.right_child:
+        print("traversing down right")
         return simple_search(root_node.right_child, value)
 
 
 def find_smallest_right(sub_root_node):
     curr_smallest_right = sub_root_node.right_child
-    while True:
-        if curr_smallest_right.left_child:
-            curr_smallest_right = curr_smallest_right.left_child
-        elif not curr_smallest_right.left_child:
-            return curr_smallest_right
+    if curr_smallest_right:
+        while True:
+            if curr_smallest_right.left_child:
+                curr_smallest_right = curr_smallest_right.left_child
+            elif not curr_smallest_right.left_child:
+                return curr_smallest_right
+    return sub_root_node
 
 
-def simple_delete(root_node, value):
-    delete_node = simple_search(root_node, value)
+def simple_delete(root_node, value=None, node=None):
+    # print(root_node)
+    # print(value)
+    if value and not node:
+        delete_node = simple_search(root_node, value)
+    if node and not value:
+        delete_node = node
+    if not delete_node:
+        print("no delete node")
+        return None
     if not delete_node.right_child and not delete_node.left_child and delete_node.parent:  # type: ignore
+        print("case 1")
         delete_node = None
         return root_node
     elif delete_node.left_child and not delete_node.right_child and delete_node.parent:  # type: ignore
+        print("case 2")
         temp_node = delete_node.left_child  # type: ignore
         delete_node.left_child.parent = delete_node.parent  # type: ignore
         if delete_node.parent.left_child == delete_node:  # type: ignore
@@ -67,6 +82,7 @@ def simple_delete(root_node, value):
         delete_node = None
         return root_node
     elif delete_node.right_child and not delete_node.left_child and delete_node.parent:  # type: ignore
+        print("case 3")
         temp_node = delete_node.right_child  # type: ignore
         delete_node.right_child.parent = delete_node.parent  # type: ignore
         if delete_node.parent.left_child == delete_node:  # type: ignore
@@ -76,12 +92,23 @@ def simple_delete(root_node, value):
         delete_node = None
         return root_node
     elif delete_node.left_child and delete_node.right_child:  # type: ignore
-        # temp_delete_node = delete_node
+        print("case 4")
         switch_node = find_smallest_right(delete_node)
-        delete_node.value = switch_node.value  # type: ignore
-        delete_node.priority = switch_node.priority  # type: ignore
-        switch_node = None
-        return root_node
+        # print(delete_node.get_info())
+        # print(switch_node.get_info())
+        delete_node.value, switch_node.value = switch_node.value, delete_node.value  # type: ignore
+        delete_node.priority, switch_node.priority = switch_node.priority, delete_node.priority  # type: ignore
+        # print(delete_node.get_info())
+        # print(switch_node.get_info())
+        # display_tree(root_node)
+        # display_tree(delete_node)
+        # display_tree(switch_node)
+        # delete_this_node_ffs = simple_search(root_node, switch_node.value)
+        # print(switch_node)
+        # print(switch_node.value)
+        # print(delete_this_node_ffs)
+        display_tree_info(root_node)
+        return simple_delete(root_node, node=switch_node)
 
 
 def get_tree(root_node):
@@ -113,6 +140,14 @@ def get_tree(root_node):
     return tree
 
 
+def display_tree_info(root_node):
+    tree = get_tree(root_node)
+    for layer in tree:
+        for node in layer:
+            if node:
+                print(node.get_info())
+
+
 def display_tree(root_node):
     tree = get_tree(root_node)
     for layer in tree:
@@ -126,15 +161,18 @@ def display_tree(root_node):
         print(layer_str)
 
 
-my_values_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+my_values_list = list(range(1, 21))
+print(my_values_list)
 random.shuffle(my_values_list)
 print(my_values_list)
 my_root_node = Node(my_values_list[0], random.random(), None, None, None)
 print(my_root_node.get_info())
-for i in range(1, 10):
+for i in range(1, len(my_values_list)):
     newest_child = simple_insert(my_root_node, my_values_list[i])
     # print(newest_child)
     print(newest_child.get_info())  # type: ignore
 
 # print(simple_search(my_root_node, random.choice(my_values_list)).get_info())  # type: ignore
+display_tree(my_root_node)
+simple_delete(my_root_node, value=random.choice(my_values_list))
 display_tree(my_root_node)
